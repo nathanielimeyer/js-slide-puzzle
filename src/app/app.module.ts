7fd6fe5fd193e41a3c2d4a4d9368e71221d2f297
Nathaniel I Meyer <nathanielimeyer@gmail.com>
2017-05-16 08:52:35 -0700

    everything working but solvability check

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { CanvasComponent } from './canvas/canvas.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CanvasComponent,
   ],
   imports: [
     BrowserModule,
